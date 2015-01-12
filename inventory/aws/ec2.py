467ba918b19a9f8ddd47ab370813ed8cfde468c7
Lénaïc Huard <lenaic@lhuard.fr>
2015-01-12 09:51:30 +0100

    Explicitely use python2
    Some distributions are using python3 as the default python.
    On those ones, we need to explicitely use python2.

--- inventory/aws/ec2.py
+++ inventory/aws/ec2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 '''
 EC2 external inventory script
--- inventory/gce/gce.py
+++ inventory/gce/gce.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # Copyright 2013 Google Inc.
 #
 # This file is part of Ansible
