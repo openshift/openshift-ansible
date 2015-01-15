bc8ebc701581e19d2e8cc9b334e155ae4e68367b
Jason DeTiberus <jdetiber@redhat.com>
2015-01-15 17:37:01 -0500

    add ability to use creds files for aws creds

--- lib/aws_command.rb
+++ lib/aws_command.rb
@@ -19,44 +19,28 @@ module OpenShift
              :desc => 'The tag(s) to add to the new instances. Allowed characters are letters, numbers, and hyphens.'
       desc "launch", "Launches instances."
       def launch()
-        AwsHelper.check_creds()
-
-        # Expand all of the instance names so that we have a complete array
-        names = []
-        options[:count].times { names << "#{options[:env]}-#{options[:type]}-#{SecureRandom.hex(5)}" }
+        aws_helper = AwsHelper.new(:launch, options)
 
         ah = AnsibleHelper.for_aws()
-
-        # AWS specific configs
-        ah.extra_vars['oo_new_inst_names'] = names
-        ah.extra_vars['oo_new_inst_tags'] = options[:tag]
-        ah.extra_vars['oo_env'] = options[:env]
-
-        # Add a created by tag
-        ah.extra_vars['oo_new_inst_tags'] = {} if ah.extra_vars['oo_new_inst_tags'].nil?
-
-        ah.extra_vars['oo_new_inst_tags']['created-by'] = ENV['USER']
-        ah.extra_vars['oo_new_inst_tags'].merge!(AwsHelper.generate_env_tag(options[:env]))
-        ah.extra_vars['oo_new_inst_tags'].merge!(AwsHelper.generate_host_type_tag(options[:type]))
-        ah.extra_vars['oo_new_inst_tags'].merge!(AwsHelper.generate_env_host_type_tag(options[:env], options[:type]))
+        ah.extra_vars = aws_helper.extra_vars
 
         puts
         puts "Creating #{options[:count]} #{options[:type]} instance(s) in AWS..."
         ah.ignore_bug_6407
 
         # Make sure we're completely up to date before launching
-        clear_cache()
+        aws_helper.clear_inventory_cache
         ah.run_playbook("playbooks/aws/#{options[:type]}/launch.yml")
       ensure
         # This is so that if we a config right after a launch, the newly launched instances will be
         # in the list.
-        clear_cache()
+        aws_helper.clear_inventory_cache
       end
 
       desc "clear-cache", 'Clear the inventory cache'
       def clear_cache()
         print "Clearing inventory cache... "
-        AwsHelper.clear_inventory_cache()
+        AwsHelper.new.clear_inventory_cache
         puts "Done."
       end
 
@@ -68,26 +52,21 @@ module OpenShift
              :desc => 'The type of the instances to configure.'
       desc "config", 'Configures instances.'
       def config()
-        ah = AnsibleHelper.for_aws()
-
-        abort 'Error: you can\'t specify both --name and --type' unless options[:type].nil? || options[:name].nil?
-
-        abort 'Error: you can\'t specify both --name and --env' unless options[:env].nil? || options[:name].nil?
-
-        host_type = nil
+        config_task = nil
         if options[:name]
-          details = AwsHelper.get_host_details(options[:name])
-          ah.extra_vars['oo_host_group_exp'] = options[:name]
-          ah.extra_vars['oo_env'] = details['env']
-          host_type = details['host-type']
+          abort 'Error you can\'t specify --type or --env when you specify --name' if options[:type] || options[:env]
+          config_task = :config_host
         elsif options[:type] && options[:env]
-          oo_env_host_type_tag = AwsHelper.generate_env_host_type_tag_name(options[:env], options[:type])
-          ah.extra_vars['oo_host_group_exp'] = "groups['#{oo_env_host_type_tag}']"
-          ah.extra_vars['oo_env'] = options[:env]
-          host_type = options[:type]
+          abort 'Error you can\'t specify --name when you specify --type and --env' if options[:name]
+          config_task = :config_type_env
         else
           abort 'Error: you need to specify either --name or (--type and --env)'
         end
+
+        aws_helper = AwsHelper.new(config_task, options)
+        ah = AnsibleHelper.for_aws()
+
+        host_type = options[:name].nil? ? options[:type] : aws_helper.get_host_type(options[:name])
 
         puts
         puts "Configuring #{options[:type]} instance(s) in AWS..."
@@ -100,10 +79,7 @@ module OpenShift
              :desc => 'The environment to list.'
       desc "list", "Lists instances."
       def list()
-        AwsHelper.check_creds()
-        hosts = AwsHelper.get_hosts()
-
-        hosts.delete_if { |h| h.env != options[:env] } unless options[:env].nil?
+        hosts = AwsHelper.new(:list, options).get_hosts
 
         fmt_str = "%34s %5s %8s %17s %7s"
 
@@ -121,7 +97,7 @@ module OpenShift
           host = $2
         end
 
-        details = AwsHelper.get_host_details(host)
+        details = AwsHelper.new(:ssh, options).get_host_details(host)
         abort "\nError: Instance [#{host}] is not RUNNING\n\n" unless details['ec2_state'] == 'running'
 
         cmd = "ssh #{ssh_ops.join(' ')}"
