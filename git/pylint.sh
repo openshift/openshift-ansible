e36ca950f0bc83c844e3c368bb64845ea51f3ea2
Kenny Woodson <kwoodson@redhat.com>
2015-04-22 17:16:46 -0400

    Testing jenkins hooks

--- git/pylint.sh
+++ git/pylint.sh
@@ -5,8 +5,10 @@ OLDREV=$1
 NEWREV=$2
 TRG_BRANCH=$3
 
+PYTHON=/var/lib/jenkins/python27/bin/python
+
 /usr/bin/git diff --name-only $OLDREV $NEWREV --diff-filter=ACM | \
  grep ".py$" | \
- xargs -r -I{} pylint --rcfile ${WORKSPACE}/git/.pylintrc  {}
+ xargs -r -I{} ${PYTHON} -m pylint --rcfile ${WORKSPACE}/git/.pylintrc  {}
 
 exit $?
