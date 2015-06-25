10f59cf0dd4c89ead7244d61f0e91e5a43183cd0
Lénaïc Huard <lhuard@amadeus.com>
2015-06-25 11:57:26 +0200

    Put the license inside the source file and add pylint ignore statement for what the Ansible API imposes

--- ansible-profile/callback_plugins/profile_tasks.py
+++ ansible-profile/callback_plugins/profile_tasks.py
@@ -1,3 +1,31 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2014 Jharrod LaFon
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'''
+A plugin for timing tasks
+
+This plugin records the time spent in each task.
+At the end of the playbook, it displays the time spent in the 10 slowest tasks.
+'''
+
 import time
 
 
@@ -9,6 +37,9 @@ class CallbackModule(object):
         self.stats = {}
         self.current = None
 
+    # Reason: The is_conditional parameter is part of the Ansible plugin API
+    # Status: permanently disabled
+    # pylint: disable=unused-argument
     def playbook_on_task_start(self, name, is_conditional):
         """
         Logs the start of each task
@@ -21,6 +52,9 @@ class CallbackModule(object):
         self.current = name
         self.stats[self.current] = time.time()
 
+    # Reason: The stats parameter is part of the Ansible plugin API
+    # Status: permanently disabled
+    # pylint: disable=unused-argument
     def playbook_on_stats(self, stats):
         """
         Prints the timings
