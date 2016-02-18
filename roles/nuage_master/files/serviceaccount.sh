3a042592a0f8e6e31313e5c636b403778c34dd73
Vishal Patil <vishal.patil@nuagenetworks.net>
2016-02-18 18:25:20 -0500

    Changes required for Nuage Monitor REST server

--- roles/nuage_master/files/serviceaccount.sh
+++ roles/nuage_master/files/serviceaccount.sh
@@ -15,6 +15,13 @@ for i in "$@"; do
         --output-cert-dir=*)
             OUTDIR="${i#*=}"
             CONFIG_FILE=${OUTDIR}/nuage.kubeconfig
+            REST_SERVER_KEY=${OUTDIR}/nuageMonServer.key
+            REST_SERVER_CERT=${OUTDIR}/nuageMonServer.crt
+            REST_CLIENT_KEY=${OUTDIR}/nuageMonClient.key
+            REST_CLIENT_CERT=${OUTDIR}/nuageMonClient.crt
+            REST_CA_KEY=${OUTDIR}/nuageMonCA.key
+            REST_CA_CERT=${OUTDIR}/nuageMonCA.crt
+            REST_CA_SERIAL=${OUTDIR}/nuageMonCA.serial.txt
         ;;
     esac
 done
@@ -59,5 +66,31 @@ oadm create-api-client-config --certificate-authority=${CA_CERT} --client-dir=${
 # Verify the finalized kubeconfig
 if ! [ $(oc whoami --config=$CONFIG_FILE) == 'system:serviceaccounts:default:nuage' ]; then
     echo "Service account creation failed!"
-    exit 1
 fi
+
+
+openssl genrsa -out $REST_CA_KEY 4096
+openssl req -new -x509 -key $REST_CA_KEY -out $REST_CA_CERT -subj "/CN=nuage-signer"
+echo '00' > $REST_CA_SERIAL
+
+OPENSSL_CONFIG='
+[ clientauth ]
+basicConstraints=CA:FALSE
+extendedKeyUsage=critical,clientAuth
+'
+
+echo "$OPENSSL_CONFIG" > ${OUTDIR}/openssl.cnf
+openssl genrsa -out $REST_SERVER_KEY 4096
+openssl req -key $REST_SERVER_KEY -new -out ${OUTDIR}/restServer.req -subj "/CN=$(hostname -f)"
+openssl x509 -req -in ${OUTDIR}/restServer.req -CA $REST_CA_CERT -CAkey \
+    $REST_CA_KEY -CAserial $REST_CA_SERIAL \
+    -out $REST_SERVER_CERT
+
+openssl genrsa -out $REST_CLIENT_KEY 4096
+openssl req -key $REST_CLIENT_KEY -new -out ${OUTDIR}/restClient.req -subj "/CN=nuage-client"
+openssl x509 -req -in ${OUTDIR}/restClient.req -CA $REST_CA_CERT -CAkey \
+    $REST_CA_KEY -CAserial $REST_CA_SERIAL \
+    -out $REST_CLIENT_CERT -extensions clientauth \
+    -extfile ${OUTDIR}/openssl.cnf
+
+rm ${OUTDIR}/{restServer,restClient}.req
