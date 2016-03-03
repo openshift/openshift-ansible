9aff21b6fe095408cd75475d0371d88508d26819
Joel Diaz <jdiaz@redhat.com>
2016-03-03 14:50:25 -0500

    change lib_zabbix's import to new pathing

--- roles/lib_zabbix/library/zbx_trigger.py
+++ roles/lib_zabbix/library/zbx_trigger.py
@@ -28,7 +28,7 @@ ansible module for zabbix triggers
 # pylint: disable=duplicate-code
 
 # pylint: disable=import-error
-from openshift_tools.monitoring.zbxapi import ZabbixAPI, ZabbixConnection
+from openshift_tools.zbxapi import ZabbixAPI, ZabbixConnection
 
 def exists(content, key='result'):
     ''' Check if key exists in content or the size of content[key] > 0
