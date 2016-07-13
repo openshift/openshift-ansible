41d6b3c3d2277c3021c0432fa812490a09cb5f5a
Jorge Tudela <jtudelag@redhat.com>
2016-07-13 09:54:09 +0200

    Default sdn_cluster_network_cidr changed to 10.128.0.0/14

--- roles/openshift_facts/library/openshift_facts.py
+++ roles/openshift_facts/library/openshift_facts.py
@@ -909,7 +909,7 @@ def set_sdn_facts_if_unset(facts, system_facts):
 
     if 'master' in facts:
         if 'sdn_cluster_network_cidr' not in facts['master']:
-            facts['master']['sdn_cluster_network_cidr'] = '10.1.0.0/16'
+            facts['master']['sdn_cluster_network_cidr'] = '10.128.0.0/14'
         if 'sdn_host_subnet_length' not in facts['master']:
             facts['master']['sdn_host_subnet_length'] = '8'
 
