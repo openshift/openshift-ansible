eda9e2190db0c9c52ba6ce7008355dc0c2d6e67a
Ben Parees <bparees@redhat.com>
2016-09-13 16:16:10 +0200

    get quickstarts from origin, not upstream example repos

--- roles/openshift_examples/examples-sync.sh
+++ roles/openshift_examples/examples-sync.sh
@@ -14,27 +14,13 @@ TEMP=`mktemp -d`
 pushd $TEMP
 
 wget https://github.com/openshift/origin/archive/master.zip -O origin-master.zip
-wget https://github.com/openshift/django-ex/archive/master.zip -O django-ex-master.zip
-wget https://github.com/openshift/rails-ex/archive/master.zip -O rails-ex-master.zip
-wget https://github.com/openshift/nodejs-ex/archive/master.zip -O nodejs-ex-master.zip
-wget https://github.com/openshift/dancer-ex/archive/master.zip -O dancer-ex-master.zip
-wget https://github.com/openshift/cakephp-ex/archive/master.zip -O cakephp-ex-master.zip
 wget https://github.com/jboss-openshift/application-templates/archive/${XPAAS_VERSION}.zip -O application-templates-master.zip
 unzip origin-master.zip
-unzip django-ex-master.zip
-unzip rails-ex-master.zip
-unzip nodejs-ex-master.zip
-unzip dancer-ex-master.zip
-unzip cakephp-ex-master.zip
 unzip application-templates-master.zip
 cp origin-master/examples/db-templates/* ${EXAMPLES_BASE}/db-templates/
+cp origin-master/examples/quickstarts/* ${EXAMPLES_BASE}/quickstart-templates/
 cp origin-master/examples/jenkins/jenkins-*template.json ${EXAMPLES_BASE}/quickstart-templates/
 cp origin-master/examples/image-streams/* ${EXAMPLES_BASE}/image-streams/
-cp django-ex-master/openshift/templates/* ${EXAMPLES_BASE}/quickstart-templates/
-cp rails-ex-master/openshift/templates/* ${EXAMPLES_BASE}/quickstart-templates/
-cp nodejs-ex-master/openshift/templates/* ${EXAMPLES_BASE}/quickstart-templates/
-cp dancer-ex-master/openshift/templates/* ${EXAMPLES_BASE}/quickstart-templates/
-cp cakephp-ex-master/openshift/templates/* ${EXAMPLES_BASE}/quickstart-templates/
 mv application-templates-${XPAAS_VERSION}/jboss-image-streams.json ${EXAMPLES_BASE}/xpaas-streams/
 find application-templates-${XPAAS_VERSION}/ -name '*.json' ! -wholename '*secret*' ! -wholename '*demo*' -exec mv {} ${EXAMPLES_BASE}/xpaas-templates/ \;
 wget https://raw.githubusercontent.com/jboss-fuse/application-templates/master/fis-image-streams.json          -O ${EXAMPLES_BASE}/xpaas-streams/fis-image-streams.json
