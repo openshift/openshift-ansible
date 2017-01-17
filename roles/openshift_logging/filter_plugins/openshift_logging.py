598b2652ac9bfe94622cbe6324d4f121bf996c70
ewolinetz <ewolinet@redhat.com>
2017-01-17 14:42:41 -0600

    Addressing Travis errors

--- roles/openshift_logging/filter_plugins/openshift_logging.py
+++ roles/openshift_logging/filter_plugins/openshift_logging.py
@@ -1,28 +1,37 @@
-import random, string
-import shutil
-import sys
-import StringIO
+'''
+ Openshift Logging class that provides useful filters used in Logging
+'''
 
-def random_word(source_alpha,length):
+import random
+
+
+def random_word(source_alpha, length):
+    ''' Returns a random word given the source of characters to pick from and resulting length '''
     return ''.join(random.choice(source_alpha) for i in range(length))
 
+
 def entry_from_named_pair(register_pairs, key):
-    from ansible.utils.display import Display
+    ''' Returns the entry in key given results provided by register_pairs '''
     results = register_pairs.get("results")
-    if results == None:
-        raise RuntimeError("The dict argument does not have a 'results' entry.  Must not have been created using 'register' in a loop")
+    if results is None:
+        raise RuntimeError("The dict argument does not have a 'results' entry. "
+                           "Must not have been created using 'register' in a loop")
     for result in results:
         item = result.get("item")
-        if item != None:
-            name = item.get("name") 
+        if item is not None:
+            name = item.get("name")
             if name == key:
                 return result["content"]
     raise RuntimeError("There was no entry found in the dict that had an item with a name that matched {}".format(key))
 
+
+# pylint: disable=too-few-public-methods
 class FilterModule(object):
     ''' OpenShift Logging Filters '''
 
+    # pylint: disable=no-self-use, too-few-public-methods
     def filters(self):
+        ''' Returns the names of the filters provided by this class '''
         return {
             'random_word': random_word,
             'entry_from_named_pair': entry_from_named_pair,
