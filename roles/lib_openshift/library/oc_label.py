3aa123c5b2b145e57776e297ac62e133b3f6993e
Thomas Wiest <twiest@redhat.com>
2017-02-07 16:39:51 -0500

    Fixed ansible module unit and integration tests and added runners.

--- roles/lib_openshift/library/oc_label.py
+++ roles/lib_openshift/library/oc_label.py
@@ -1552,7 +1552,7 @@ def main():
             state=dict(default='present', type='str',
                        choices=['present', 'absent', 'list', 'add']),
             debug=dict(default=False, type='bool'),
-            kind=dict(default='node', type='str', required=True,
+            kind=dict(default='node', type='str',
                       choices=['node', 'pod', 'namespace']),
             name=dict(default=None, type='str'),
             namespace=dict(default=None, type='str'),
