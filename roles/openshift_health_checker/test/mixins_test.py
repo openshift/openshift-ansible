209810c9905c0ebf89b1e03ccb8593f78a6e5396
Rodolfo Carvalho <rhcarvalho@gmail.com>
2017-03-22 21:05:18 +0100

    Add unit tests for mixins.py

---
+++ roles/openshift_health_checker/test/mixins_test.py
@@ -0,0 +1,23 @@
+import pytest
+
+from openshift_checks import OpenShiftCheck, OpenShiftCheckException
+from openshift_checks.mixins import NotContainerizedMixin
+
+
+class NotContainerizedCheck(NotContainerizedMixin, OpenShiftCheck):
+    name = "not_containerized"
+    run = NotImplemented
+
+
+@pytest.mark.parametrize('task_vars,expected', [
+    (dict(openshift=dict(common=dict(is_containerized=False))), True),
+    (dict(openshift=dict(common=dict(is_containerized=True))), False),
+])
+def test_is_active(task_vars, expected):
+    assert NotContainerizedCheck.is_active(task_vars) == expected
+
+
+def test_is_active_missing_task_vars():
+    with pytest.raises(OpenShiftCheckException) as excinfo:
+        NotContainerizedCheck.is_active(task_vars={})
+    assert 'is_containerized' in str(excinfo.value)
