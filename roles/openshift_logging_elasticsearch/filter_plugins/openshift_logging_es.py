8832e6a37aebd1777f13e578d1097a539143eaad
Jan Wozniak <jwozniak@redhat.com>
2017-05-29 17:44:41 +0200

    fix tox warnings

--- roles/openshift_logging_elasticsearch/filter_plugins/openshift_logging_es.py
+++ roles/openshift_logging_elasticsearch/filter_plugins/openshift_logging_es.py
@@ -1,26 +1,33 @@
 '''
  Class providing lower bound filter to prevent users accidentally throttling their pods
 '''
+
 import re
 
-def norm(x, m):
-    x = x.strip(' ')
-    match = re.search('([0-9\.]*)(.*)', x)
+
+def norm(value, prefix_map):
+    '''Normalize value based on the prefix_map'''
+    value = value.strip(' ')
+    match = re.search('([0-9.]*)(.*)', value)
     val = match.group(1)
     unit = match.group(2).strip(' ')
-    if unit not in m:
+    if unit not in prefix_map:
         raise ValueError('Unit ' + unit + ' not supported')
-    return float(val) * m[unit]
+    return float(val) * prefix_map[unit]
 
-def norm_cpu(x):
-    m = {
-        '':  1,
-        'm': 1e-3
+
+def norm_cpu(value):
+    '''Normalize cpu value'''
+    prefix_map = {
+        '': 1,
+        'prefix_map': 1e-3
     }
-    return norm(x, m)
-    
-def norm_mem(x):
-    m = {
+    return norm(value, prefix_map)
+
+
+def norm_mem(value):
+    '''Normalize memory value'''
+    prefix_map = {
         'E': 1e18,
         'P': 1e15,
         'T': 1e12,
@@ -33,22 +40,32 @@ def norm_mem(x):
         'Gi': 1e9,
         'Mi': 1e6,
         'Ki': 1e3,
-        '':  1,
+        '': 1,
     }
-    return norm(x, m)
+    return norm(value, prefix_map)
 
-def lower_bound_cpu(x, limit):
-    if not x:
-        return x
-    return max((norm_cpu(x), x), (norm_cpu(limit), limit))[1]
 
-def lower_bound_mem(x, limit):
-    if not x:
-        return x
-    return max((norm_mem(x), x), (norm_mem(limit), limit))[1]
-    
+def lower_bound_cpu(value, limit):
+    '''Set return max(value, limit) respecting the unit conversions'''
+    if not value:
+        return value
+    return max((norm_cpu(value), value), (norm_cpu(limit), limit))[1]
+
+
+def lower_bound_mem(value, limit):
+    '''Set return max(value, limit) respecting the unit conversions'''
+    if not value:
+        return value
+    return max((norm_mem(value), value), (norm_mem(limit), limit))[1]
+
+
+# pylint: disable=too-few-public-methods
 class FilterModule(object):
+    ''' OpenShift Logging Filters for ES '''
+
+    # pylint: disable=no-self-use, too-few-public-methods
     def filters(self):
+        ''' Returns the names of the filters provided by this class '''
         return {
             'lower_bound_cpu': lower_bound_cpu,
             'lower_bound_mem': lower_bound_mem
