1b81e5432b77218230bfeb6152dadd0ff0933d2f
Scott Dodson <sdodson@redhat.com>
2017-06-30 11:18:06 -0400

    Bump xPaas v1.4.0 for v3.6

--- roles/openshift_examples/examples-sync.sh
+++ roles/openshift_examples/examples-sync.sh
@@ -5,7 +5,7 @@
 #
 # This script should be run from openshift-ansible/roles/openshift_examples
 
-XPAAS_VERSION=ose-v1.3.6
+XPAAS_VERSION=ose-v1.4.0
 ORIGIN_VERSION=${1:-v3.6}
 RHAMP_TAG=2.0.0.GA
 EXAMPLES_BASE=$(pwd)/files/examples/${ORIGIN_VERSION}
