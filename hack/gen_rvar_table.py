1dc26f6ce446b7794ce27bca6f90516baa59d98c
Jan Chaloupka <jchaloup@redhat.com>
2017-08-04 19:28:55 +0200

    - incorporate review nits - extend with optparser to choose between --table and --checks output - add requires_when support

--- hack/gen_rvar_table.py
+++ hack/gen_rvar_table.py
@@ -1,28 +1,60 @@
-#!/bin/python
-
+#!/usr/bin/env python
+from __future__ import print_function
 import re
+import jinja2
+import optparse
+import logging
 
-with open('defaults/main.yml') as f:
-    lines = f.read().split("\n")
+
+def setOptions():
+
+	parser = optparse.OptionParser("%prog ")
+
+	parser.add_option(
+	    "", "-d", "--defaults", dest="defaults", default = "",
+	    help = "Location of the defaults/main.yml file"
+	)
+
+	parser.add_option(
+	    "", "-t", "--table", dest="table", default = False, action="store_true",
+	    help = "Generate role variable table"
+	)
+
+	parser.add_option(
+	    "", "-c", "--checks", dest="checks", default = False, action="store_true",
+	    help = "Generate a list of check tasks"
+	)
+
+	return parser
+
+def checkOptions(options):
+
+	if options.defaults == "":
+		logging.error("--defaults missing")
+		exit(1)
+
+	if not options.table and not options.checks:
+		logging.error("at least one of --table|--checks needs to be set")
+		exit(1)
 
 def print_rvar_table(rvar_table):
-    keys = ["name", "description", "default", "required"]
+    keys = ("name", "description", "default", "required")
 
     sizes = {}
     for key in keys:
         sizes[key] = len(key)
 
     for item in rvar_table:
-        for key in item.keys():
+        for key in keys:
             sizes[key] = max(sizes[key], len(item[key]))
 
     headers = []
     delimiters = []
     for key in keys:
-        headers.append(" {}{} ".format(key, " "*(sizes[key]-len(key))))
-        delimiters.append("-{}-".format("-"*(sizes[key])))
-    print "|{}|".format("|".join(headers))
-    print "|{}|".format("|".join(delimiters))
+        headers.append(" " + key.ljust(sizes[key] + 1))
+        delimiters.append("-"*(sizes[key] + 2))
+    print("|{}|".format("|".join(headers)))
+    print("|{}|".format("|".join(delimiters)))
 
     # sort items by variable name
     ki_items = {}
@@ -33,8 +65,48 @@ def print_rvar_table(rvar_table):
         item = ki_items[key]
         data = []
         for key in keys:
-            data.append(" {}{} ".format(item[key], " "*(sizes[key]-len(item[key]))))
-        print "|{}|".format("|".join(data))
+            data.append(" " + item[key].ljust(sizes[key] + 1))
+        print("|{}|".format("|".join(data)))
+
+required_template_task_str = """
+- name: "Fail if {{ role_variable_name }} is not defined"
+  fail:
+    msg: "{{ role_variable_name }} must be specified for this role"
+  when:
+  - {{ role_variable_name }} is not defined
+"""
+
+required_when_template_task_str = """
+- name: "Fail if {{ role_variable_name }} is not defined"
+  fail:
+    msg: "{{ role_variable_name }} must be specified for this role"
+  when:
+  - {{ role_variable_name }} is not defined
+  {%- for condition in role_variable_condition %}
+  - {{ condition -}}
+  {% endfor %}
+"""
+
+def generate_check_tasks(rvar_table):
+    checks = []
+    for item in rvar_table:
+        if item["required"]:
+            template_vars = {
+                "role_variable_name": item["name"]
+            }
+            # cause the PyYAML will reorder the keys
+            checks.append(jinja2.Environment().from_string(required_template_task_str).render(template_vars))
+            continue
+        if item["required_when"]:
+            for conditions in item["required_when"]:
+                template_vars = {
+                    "role_variable_name": item["name"],
+                    "role_variable_condition": conditions,
+                }
+                # cause the PyYAML will reorder the keys
+                checks.append(jinja2.Environment().from_string(required_when_template_task_str).render(template_vars))
+    print("\n".join(checks))
+
 
 def get_delims(line, skip_list=False):
     # replace all "...", '...' and [...] with # so it is easy to determine
@@ -67,7 +139,9 @@ def parse_annotations(line):
     annotations = {
         # set false to empty to make the column more transparent
         "required": "",
-        "description": ""
+        "description": "",
+        "default": "",
+        "required_when": [],
     }
 
     pair_start = 0
@@ -82,38 +156,52 @@ def parse_annotations(line):
             annotations["description"] = value[1:-1]
         elif key == "choices":
             annotations["choices"] = parse_list(value)
+        elif key == "name":
+            annotations["name"] = value
+        elif key == "required_when":
+            annotations["required_when"].append(parse_list(value))
 
         pair_start = pair_end + 1
-
     return annotations
 
-rvar_table = []
+if __name__ == "__main__":
 
-last_comment = ""
-for line in lines:
-    if line.startswith("#:"):
-        last_comment = line
-        continue
-    # default role variable definition
-    if line.startswith("r_"):
-        parts = line.split(":")
-        default = parts[1].strip()
+    options, args = setOptions().parse_args()
+    checkOptions(options)
 
-        if last_comment != "":
-            annotations = parse_annotations(last_comment)
-            if default == "\"{{ omit }}\"":
-                default = ""
+    with open(options.defaults) as f:
+        lines = f.read().split("\n")
 
-            if annotations["required"] == "false":
-                annotations["required"] = ""
 
-            rvar_table.append({
-                "name": parts[0],
-                "description": annotations["description"],
-                "default": default,
-                "required": annotations["required"],
-            })
-            last_comment = ""
-        continue
+    rvar_table = []
 
-print_rvar_table(rvar_table)
+    annotations = {}
+    for line in lines:
+        if line.startswith("#:"):
+            annotations = parse_annotations(line)
+            if "name" in annotations:
+                if annotations["required"] == "false":
+                    annotations["required"] = ""
+
+                rvar_table.append(annotations)
+                annotations = {}
+            continue
+        # default role variable definition
+        if line.startswith("r_"):
+            parts = line.split(":")
+            default = parts[1].strip()
+
+            if annotations != {}:
+                if annotations["required"] == "false":
+                    annotations["required"] = ""
+
+                annotations["name"] = parts[0]
+
+                rvar_table.append(annotations)
+                last_annotations = {}
+            continue
+
+    if options.table:
+        print_rvar_table(rvar_table)
+    elif options.checks:
+        generate_check_tasks(rvar_table)
