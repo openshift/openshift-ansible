ac62ea0066934877f94e99bda6ec53a9c03ababb
Tim Bielawa <tbielawa@redhat.com>
2017-10-20 16:17:41 -0400

    Fix lint

--- roles/openshift_management/filter_plugins/oo_management_filters.py
+++ roles/openshift_management/filter_plugins/oo_management_filters.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+Filter methods for the management role
+"""
+
 
 def oo_filter_container_providers(results):
     """results - the result from posting the API calls for adding new
@@ -8,13 +12,14 @@ providers"""
     for result in results:
         if 'results' in result['json']:
             # We got an OK response
-            r = result['json']['results'][0]
-            all_results.append("Provider '{}' - Added successfully".format(r['name']))
+            res = result['json']['results'][0]
+            all_results.append("Provider '{}' - Added successfully".format(res['name']))
         elif 'error' in result['json']:
             # This was a problem
             all_results.append("Provider '{}' - Failed to add. Message: {}".format(
                 result['item']['name'], result['json']['error']['message']))
     return all_results
+
 
 class FilterModule(object):
     """ Custom ansible filter mapping """
