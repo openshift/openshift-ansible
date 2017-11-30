2c65e694ba24d14aa16c2b3de5e8f463f0359135
Scott Dodson <sdodson@redhat.com>
2017-11-30 13:28:29 -0500

    Add v3.9 support

--- roles/openshift_facts/library/openshift_facts.py
+++ roles/openshift_facts/library/openshift_facts.py
@@ -761,16 +761,21 @@ def set_version_facts_if_unset(facts):
             version_gte_3_6 = version >= LooseVersion('3.6')
             version_gte_3_7 = version >= LooseVersion('3.7')
             version_gte_3_8 = version >= LooseVersion('3.8')
+            version_gte_3_9 = version >= LooseVersion('3.9')
         else:
             # 'Latest' version is set to True, 'Next' versions set to False
             version_gte_3_6 = True
             version_gte_3_7 = True
             version_gte_3_8 = False
+            version_gte_3_9 = False
         facts['common']['version_gte_3_6'] = version_gte_3_6
         facts['common']['version_gte_3_7'] = version_gte_3_7
         facts['common']['version_gte_3_8'] = version_gte_3_8
+        facts['common']['version_gte_3_9'] = version_gte_3_9
 
-        if version_gte_3_8:
+        if version_gte_3_9:
+            examples_content_version = 'v3.9'
+        elif version_gte_3_8:
             examples_content_version = 'v3.8'
         elif version_gte_3_7:
             examples_content_version = 'v3.7'
--- roles/openshift_master_facts/test/openshift_master_facts_default_priorities_tests.py
+++ roles/openshift_master_facts/test/openshift_master_facts_default_priorities_tests.py
@@ -44,7 +44,7 @@ DEFAULT_PRIORITIES_1_5 = [
 
 DEFAULT_PRIORITIES_3_6 = DEFAULT_PRIORITIES_1_5
 
-DEFAULT_PRIORITIES_3_7 = DEFAULT_PRIORITIES_3_6
+DEFAULT_PRIORITIES_3_9 = DEFAULT_PRIORITIES_3_8 = DEFAULT_PRIORITIES_3_7 = DEFAULT_PRIORITIES_3_6
 
 ZONE_PRIORITY = {
     'name': 'Zone',
@@ -71,6 +71,10 @@ TEST_VARS = [
     ('3.6', 'openshift-enterprise', DEFAULT_PRIORITIES_3_6),
     ('3.7', 'origin', DEFAULT_PRIORITIES_3_7),
     ('3.7', 'openshift-enterprise', DEFAULT_PRIORITIES_3_7),
+    ('3.8', 'origin', DEFAULT_PRIORITIES_3_8),
+    ('3.8', 'openshift-enterprise', DEFAULT_PRIORITIES_3_8),
+    ('3.9', 'origin', DEFAULT_PRIORITIES_3_9),
+    ('3.9', 'openshift-enterprise', DEFAULT_PRIORITIES_3_9),
 ]
 
 
