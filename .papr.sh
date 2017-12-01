6e1b4eea99c5d65da2a15f238fc4b84bd53937c2
Scott Dodson <sdodson@redhat.com>
2017-12-01 15:05:16 -0500

    Update papr on release-3.6

--- .papr.sh
+++ .papr.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -xeuo pipefail
 
+# Essentially use a similar procedure other openshift-ansible PR tests use to
+# determine which image tag should be used. This allows us to avoid hardcoding a
+# specific version which quickly becomes stale.
+
+if [ -n "${PAPR_BRANCH:-}" ]; then
+    target_branch=$PAPR_BRANCH
+else
+    target_branch=$PAPR_PULL_TARGET_BRANCH
+fi
+
+# this is a bit wasteful, though there's no easy way to say "only clone up to
+# the first tag in the branch" -- ideally, PAPR could help with caching here
+git clone --branch $target_branch --single-branch https://github.com/openshift/origin
+export OPENSHIFT_IMAGE_TAG=$(git -C origin describe --abbrev=0)
+
 echo "Targeting OpenShift Origin $OPENSHIFT_IMAGE_TAG"
 
 pip install -r requirements.txt
@@ -22,11 +37,15 @@ trap upload_journals ERR
 # https://github.com/openshift/openshift-ansible/issues/4478 is fixed.
 ansible-playbook -vvv -i .papr.inventory playbooks/byo/config.yml -e "openshift_image_tag=$OPENSHIFT_IMAGE_TAG"
 
-# run a small subset of origin conformance tests to sanity
-# check the cluster NB: we run it on the master since we may
-# be in a different OSP network
-ssh ocp-master docker run --rm --net=host --privileged \
-  -v /etc/origin/master/admin.kubeconfig:/config fedora:25 sh -c \
-    '"dnf install -y origin-tests && \
-      KUBECONFIG=/config /usr/libexec/origin/extended.test --ginkgo.v=1 \
-        --ginkgo.noColor --ginkgo.focus=\"Services.*NodePort|EmptyDir\""'
+### DISABLING TESTS FOR NOW, SEE:
+### https://github.com/openshift/openshift-ansible/pull/6132
+
+### # run a small subset of origin conformance tests to sanity
+### # check the cluster NB: we run it on the master since we may
+### # be in a different OSP network
+### ssh ocp-master docker run --rm --net=host --privileged \
+###   -v /etc/origin/master/admin.kubeconfig:/config \
+###   registry.fedoraproject.org/fedora:27 sh -c \
+###     '"dnf install -y origin-tests && \
+###       KUBECONFIG=/config /usr/libexec/origin/extended.test --ginkgo.v=1 \
+###         --ginkgo.noColor --ginkgo.focus=\"Services.*NodePort|EmptyDir\""'
