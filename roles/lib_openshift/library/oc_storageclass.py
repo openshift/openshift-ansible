ffc860e4715b63a6b2b2a9011de049be1c9f0ccd
Fabian von Feilitzsch <fabian@fabianism.us>
2018-03-16 14:40:42 -0400

    remove duplicate time import

--- roles/lib_openshift/library/oc_storageclass.py
+++ roles/lib_openshift/library/oc_storageclass.py
@@ -1595,7 +1595,6 @@ class OCStorageClass(OpenShiftCLI):
         self.delete()
         # pause here and attempt to wait for delete.
         # Better option would be to poll
-        import time
         time.sleep(5)
         return self.create()
 
