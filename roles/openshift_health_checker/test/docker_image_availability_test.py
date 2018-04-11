1f1eb5ddd526b48fbf3351ac88cf276215cf4db4
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-04-11 17:41:13 +0200

    health checks: use etcd_image as it would work on Origin correctly

--- roles/openshift_health_checker/test/docker_image_availability_test.py
+++ roles/openshift_health_checker/test/docker_image_availability_test.py
@@ -301,7 +301,7 @@ def test_registry_console_image(task_vars, expected):
     ), (
         dict(
             group_names=['oo_etcd_to_config'],
-            osm_etcd_image='spam/etcd',
+            etcd_image='spam/etcd',
         ),
         set(['spam/etcd']),
     ),
