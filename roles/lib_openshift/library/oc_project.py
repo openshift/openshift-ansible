d30e44487fbb36a0d0b58b5f52ba5e9d8120fb32
jupierce <jupierce@redhat.com>
2018-04-23 16:12:56 -0400

    Fix oc_version oc_short to report '3.10'

--- roles/lib_openshift/library/oc_project.py
+++ roles/lib_openshift/library/oc_project.py
@@ -1312,9 +1312,10 @@ class Utils(object):  # pragma: no cover
                 version = version.split("-")[0]
 
             if version.startswith('v'):
-                versions_dict[tech + '_numeric'] = version[1:].split('+')[0]
-                # "v3.3.0.33" is what we have, we want "3.3"
-                versions_dict[tech + '_short'] = version[1:4]
+                version = version[1:]  # Remove the 'v' prefix
+                versions_dict[tech + '_numeric'] = version.split('+')[0]
+                # "3.3.0.33" is what we have, we want "3.3"
+                versions_dict[tech + '_short'] = "{}.{}".format(*version.split('.'))
 
         return versions_dict
 
