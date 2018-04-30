1c5e7e743cafef231561f7f60765b36741b44c79
Michal Fojtik <mfojtik@redhat.com>
2018-04-30 16:35:31 +0200

    azure: add metadata server IP to no_proxy list

--- roles/openshift_facts/library/openshift_facts.py
+++ roles/openshift_facts/library/openshift_facts.py
@@ -1145,6 +1145,8 @@ def set_proxy_facts(facts):
             if 'generate_no_proxy_hosts' in common and safe_get_bool(common['generate_no_proxy_hosts']):
                 if 'no_proxy_internal_hostnames' in common:
                     common['no_proxy'].extend(common['no_proxy_internal_hostnames'].split(','))
+            # TODO: This is Azure specific and should be scoped out to only Azure installs
+            common['no_proxy'].append('169.254.169.254')
             # We always add local dns domain and ourselves no matter what
             common['no_proxy'].append('.' + common['dns_domain'])
             common['no_proxy'].append('.svc')
