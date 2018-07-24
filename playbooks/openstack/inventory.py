8f7bd27b665a4f6e190faf2d75a69338c5fec7a6
Alberto Rodriguez Peon <alberto.rodriguez.peon@cern.ch>
2018-07-24 16:38:11 +0200

    Filter host based on OPENSHIFT_CLUSTER env variable

--- playbooks/openstack/inventory.py
+++ playbooks/openstack/inventory.py
@@ -124,11 +124,11 @@ def build_inventory():
     '''Build the dynamic inventory.'''
     cloud = shade.openstack_cloud()
 
-    # TODO(shadower): filter the servers based on the `OPENSHIFT_CLUSTER`
-    # environment variable.
+    cluster_name = os.getenv('OPENSHIFT_CLUSTER', 'openshift-cluster')
     cluster_hosts = [
         server for server in cloud.list_servers()
-        if 'metadata' in server and 'clusterid' in server.metadata]
+        if 'metadata' in server and 'clusterid' in server.metadata
+        and server.metadata['clusterid'] == cluster_name]
 
     inventory = base_openshift_inventory(cluster_hosts)
 
