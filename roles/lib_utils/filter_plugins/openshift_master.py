845e90a0445339e221a6550d25e9c7a5734bf259
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-08-23 16:38:56 +0200

    Dissalow custom CA file path for providers with CA path
    This commit would ensure ldap auth provider won't rewrite any files
    in the static pod and gets placed in /etc/origin/master/
    
    Previously, before static pod deployment,
    openshift_master_identity_providers
    param for LDAP auth could specify the file, which was copied on the
    host and was used by API server, running on the host.
    
    In 3.10+ this file needs to be mounted in the API server container
    (along with the rest of configuration), so a custom path in `ca` section
    could be left on the node. This file was copied unconditionally and
    could rewrite any CA / config file.
    
    This commit would always place contents from `openshift_master_ldap_ca`
    (or file from `openshift_master_ldap_ca_file`) in
    /etc/origin/master/ldap_ca.crt, which would be mounted in a static pod.
    
    This also affects OpenID auth and request header auth

--- roles/lib_utils/filter_plugins/openshift_master.py
+++ roles/lib_utils/filter_plugins/openshift_master.py
@@ -158,6 +158,8 @@ class LDAPPasswordIdentityProvider(IdentityProviderBase):
             pref_user = self._idp['attributes'].pop('preferred_username')
             self._idp['attributes']['preferredUsername'] = pref_user
 
+        self._idp['ca'] = '/etc/origin/master/ldap_ca.crt'
+
     def validate(self):
         ''' validate this idp instance '''
         if not isinstance(self.provider['attributes'], dict):
@@ -218,6 +220,7 @@ class RequestHeaderIdentityProvider(IdentityProviderBase):
                            ['emailHeaders', 'email_headers'],
                            ['nameHeaders', 'name_headers'],
                            ['preferredUsernameHeaders', 'preferred_username_headers']]
+        self._idp['clientCA'] = '/etc/origin/master/request_header_ca.crt'
 
     def validate(self):
         ''' validate this idp instance '''
@@ -357,6 +360,8 @@ class OpenIDIdentityProvider(IdentityProviderOauthBase):
             self._idp['extraScopes'] = self._idp.pop('extra_scopes')
         if 'extra_authorize_parameters' in self._idp:
             self._idp['extraAuthorizeParameters'] = self._idp.pop('extra_authorize_parameters')
+
+        self._idp['ca'] = '/etc/origin/master/openid_ca.crt'
 
     def validate(self):
         ''' validate this idp instance '''
