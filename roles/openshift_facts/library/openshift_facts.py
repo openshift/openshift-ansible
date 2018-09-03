19807c746b2c0c83d1a41e7ed11f3c8cb7bf01b8
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-09-03 11:50:30 +0200

    Don't fetch provider facts if openshift_cloud_provider_kind is not set

--- roles/openshift_facts/library/openshift_facts.py
+++ roles/openshift_facts/library/openshift_facts.py
@@ -1030,7 +1030,9 @@ class OpenShiftFacts(object):
         roles = local_facts.keys()
 
         defaults = self.get_defaults(roles)
-        provider_facts = self.init_provider_facts()
+        provider_facts = {}
+        if 'cloudprovider' in local_facts and 'kind' in local_facts['cloudprovider']:
+            provider_facts = self.init_provider_facts()
         facts = apply_provider_facts(defaults, provider_facts)
         facts = merge_facts(facts,
                             local_facts,
