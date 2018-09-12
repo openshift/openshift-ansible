b9b8db8a08d8b0ef348975ca0bb6b3c64915429f
daxen79 <yasensim@gmail.com>
2018-09-12 10:41:44 +0300

    [release-3.10]Do not stop and delete openvswitch when NSX is used #8015 and #8134

--- roles/lib_utils/action_plugins/sanity_checks.py
+++ roles/lib_utils/action_plugins/sanity_checks.py
@@ -16,7 +16,8 @@ NET_PLUGIN_LIST = (('openshift_use_openshift_sdn', True),
                    ('openshift_use_nuage', False),
                    ('openshift_use_contiv', False),
                    ('openshift_use_calico', False),
-                   ('openshift_use_kuryr', False))
+                   ('openshift_use_kuryr', False),
+                   ('openshift_use_nsx', False))
 
 ENTERPRISE_TAG_REGEX_ERROR = """openshift_image_tag must be in the format
 v#.#[.#[.#]]. Examples: v1.2, v3.4.1, v3.5.1.3,
