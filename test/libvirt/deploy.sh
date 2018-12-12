037edf63f22a18bf60b61be70f3126bbfb895f96
Michael Gugino <mgugino@redhat.com>
2018-12-12 16:15:01 -0500

    Add inventory generation

--- test/libvirt/deploy.sh
+++ test/libvirt/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+#. installrc
 ./generate_assets.sh
 ./terraform_provision.sh
+./run_ansible.sh
--- test/libvirt/generate_assets.sh
+++ test/libvirt/generate_assets.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # run this file directly via ./generate_assets.sh; sh generate_assets.sh won't work.
-. installrc
+
+./generate_inventory.sh
 cat install-config.yml.template | envsubst > install-config.yml
 cat terraform/terraform.tfvars.template | envsubst > terraform/terraform.tfvars
