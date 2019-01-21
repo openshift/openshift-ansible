bc316cac8acf195c13fc83d75797642bcb45c813
Christian Haeusler <haeusler.christian@mac.com>
2019-01-21 15:48:33 +0100

    Do nothing if input is already of return type
    Allow the `map_*` filters to accept the input they are supposed to
    return. This allows to input to be already in the resulting format
    without failing.

--- roles/lib_utils/filter_plugins/oo_filters.py
+++ roles/lib_utils/filter_plugins/oo_filters.py
@@ -606,6 +606,10 @@ def map_from_pairs(source, delim="="):
     if source == '':
         return dict()
 
+    # Return early if if input already is a dictionary.
+    if isinstance(source, dict):
+        return source
+
     return dict(item.split(delim) for item in source.split(","))
 
 
@@ -615,6 +619,10 @@ def map_to_pairs(source, delim="="):
     # Some default selectors are empty strings.
     if source == {} or source == '':
         return str()
+
+    # Return early if if input already is a pairs string.
+    if isinstance(source, basestring):
+        return source
 
     return ','.join(["{}{}{}".format(key, delim, value) for key, value in iteritems(source)])
 
