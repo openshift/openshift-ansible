e4f4befb4d080859b71361fd85441a744ef71ed6
Miciah Masters <miciah.masters@gmail.com>
2019-01-29 23:30:26 -0500

    Update router service when its annotation changes
    This commit fixes bug 1635613.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1635613
    
    * roles/lib_openshift/library/oc_adm_router.py (Router.needs_update):
    * roles/lib_openshift/src/class/oc_adm_router.py (Router.needs_update):
    Check whether the service.alpha.openshift.io/serving-cert-secret-name
    annotation on the service differs between the user and result definitions.
    * roles/lib_openshift/src/test/unit/test_oc_adm_router.py (RouterTest): Add
    tests to verify that oc_adm_router replaces an existing deployment or
    service if an old, outdated one exists.

--- roles/lib_openshift/src/test/unit/test_oc_adm_router.py
+++ roles/lib_openshift/src/test/unit/test_oc_adm_router.py
@@ -370,6 +370,389 @@ class RouterTest(unittest.TestCase):
             mock.call(['oc', 'create', '-f', mock.ANY, '-n', 'default'], None),
             mock.call(['oc', 'create', '-f', mock.ANY, '-n', 'default'], None)])
 
+    old_deployment = '''{
+    "kind": "DeploymentConfig",
+    "apiVersion": "v1",
+    "metadata": {
+        "name": "router",
+        "labels": {
+            "router": "router"
+        }
+    },
+    "spec": {
+        "strategy": {
+            "type": "Rolling",
+            "rollingParams": {
+                "maxUnavailable": "25%",
+                "maxSurge": 0
+            },
+            "resources": {}
+        },
+        "triggers": [
+            {
+                "type": "ConfigChange"
+            }
+        ],
+        "replicas": 2,
+        "test": false,
+        "selector": {
+            "router": "router"
+        },
+        "template": {
+            "metadata": {
+                "labels": {
+                    "router": "router"
+                }
+            },
+            "spec": {
+                "volumes": [
+                    {
+                        "name": "server-certificate",
+                        "secret": {
+                            "secretName": "router-certs"
+                        }
+                    }
+                ],
+                "containers": [
+                    {
+                        "name": "router",
+                        "image": "registry.redhat.io/openshift3/ose-haproxy-router:v3.5.0.39",
+                        "ports": [
+                            {
+                                "containerPort": 80
+                            },
+                            {
+                                "containerPort": 443
+                            },
+                            {
+                                "name": "stats",
+                                "containerPort": 1936,
+                                "protocol": "TCP"
+                            }
+                        ],
+                        "env": [
+                            {
+                                "name": "DEFAULT_CERTIFICATE_DIR",
+                                "value": "/etc/pki/tls/private"
+                            },
+                            {
+                                "name": "ROUTER_EXTERNAL_HOST_HOSTNAME"
+                            },
+                            {
+                                "name": "ROUTER_EXTERNAL_HOST_HTTPS_VSERVER"
+                            },
+                            {
+                                "name": "ROUTER_EXTERNAL_HOST_HTTP_VSERVER"
+                            },
+                            {
+                                "name": "ROUTER_EXTERNAL_HOST_INSECURE",
+                                "value": "false"
+                            },
+                            {
+                                "name": "ROUTER_EXTERNAL_HOST_INTERNAL_ADDRESS"
+                            },
+                            {
+                                "name": "ROUTER_EXTERNAL_HOST_PARTITION_PATH"
+                            },
+                            {
+                                "name": "ROUTER_EXTERNAL_HOST_PASSWORD"
+                            },
+                            {
+                                "name": "ROUTER_EXTERNAL_HOST_PRIVKEY",
+                                "value": "/etc/secret-volume/router.pem"
+                            },
+                            {
+                                "name": "ROUTER_EXTERNAL_HOST_USERNAME"
+                            },
+                            {
+                                "name": "ROUTER_EXTERNAL_HOST_VXLAN_GW_CIDR"
+                            },
+                            {
+                                "name": "ROUTER_SERVICE_HTTPS_PORT",
+                                "value": "443"
+                            },
+                            {
+                                "name": "ROUTER_SERVICE_HTTP_PORT",
+                                "value": "80"
+                            },
+                            {
+                                "name": "ROUTER_SERVICE_NAME",
+                                "value": "router"
+                            },
+                            {
+                                "name": "ROUTER_SERVICE_NAMESPACE",
+                                "value": "default"
+                            },
+                            {
+                                "name": "ROUTER_SUBDOMAIN"
+                            },
+                            {
+                                "name": "STATS_PASSWORD",
+                                "value": "eSfUICQyyr"
+                            },
+                            {
+                                "name": "STATS_PORT",
+                                "value": "1936"
+                            },
+                            {
+                                "name": "STATS_USERNAME",
+                                "value": "admin"
+                            }
+                        ],
+                        "resources": {
+                            "requests": {
+                                "cpu": "100m",
+                                "memory": "256Mi"
+                            }
+                        },
+                        "volumeMounts": [
+                            {
+                                "name": "server-certificate",
+                                "readOnly": true,
+                                "mountPath": "/etc/pki/tls/private"
+                            }
+                        ],
+                        "livenessProbe": {
+                            "httpGet": {
+                                "path": "/healthz",
+                                "port": 1936,
+                                "host": "localhost"
+                            },
+                            "initialDelaySeconds": 10
+                        },
+                        "readinessProbe": {
+                            "httpGet": {
+                                "path": "/healthz",
+                                "port": 1936,
+                                "host": "localhost"
+                            },
+                            "initialDelaySeconds": 10
+                        },
+                        "imagePullPolicy": "IfNotPresent"
+                    }
+                ],
+                "nodeSelector": {
+                    "type": "infra"
+                },
+                "serviceAccountName": "router",
+                "serviceAccount": "router",
+                "hostNetwork": true,
+                "securityContext": {}
+            }
+        }
+    }
+}'''
+
+    @mock.patch('oc_adm_router.locate_oc_binary')
+    @mock.patch('oc_adm_router.Utils._write')
+    @mock.patch('oc_adm_router.Utils.create_tmpfile_copy')
+    @mock.patch('oc_adm_router.Router._run')
+    def test_with_old_deployment(self, mock_cmd, mock_tmpfile_copy, mock_write, mock_oc_binary):
+        ''' Testing create with old deployment lacking EXTENDED_VALIDATION '''
+        params = {'state': 'present',
+                  'debug': False,
+                  'namespace': 'default',
+                  'name': 'router',
+                  'default_cert': None,
+                  'cert_file': None,
+                  'key_file': None,
+                  'cacert_file': None,
+                  'labels': {"router": "router", "another-label": "val"},
+                  'ports': ['80:80', '443:443'],
+                  'images': None,
+                  'latest_images': None,
+                  'clusterip': None,
+                  'portalip': None,
+                  'session_affinity': None,
+                  'service_type': None,
+                  'kubeconfig': '/etc/origin/master/admin.kubeconfig',
+                  'replicas': 2,
+                  'selector': 'type=infra',
+                  'service_account': 'router',
+                  'router_type': None,
+                  'host_network': None,
+                  'extended_validation': True,
+                  'external_host': None,
+                  'external_host_vserver': None,
+                  'external_host_insecure': False,
+                  'external_host_partition_path': None,
+                  'external_host_username': None,
+                  'external_host_password': None,
+                  'external_host_private_key': None,
+                  'stats_user': None,
+                  'stats_password': None,
+                  'stats_port': 1936,
+                  'edits': []}
+
+        mock_cmd.side_effect = [
+            (0, RouterTest.old_deployment, ''),
+            (1, '', 'Error from server (NotFound): service "router" not found'),
+            (1, '', 'Error from server (NotFound): serviceaccount "router" not found'),
+            (1, '', 'Error from server (NotFound): secret "router-certs" not found'),
+            (1, '', 'Error from server (NotFound): clusterrolebinding "router-router-role" not found'),
+            (0, RouterTest.dry_run, ''),
+            (0, '', ''),
+            (0, '', ''),
+            (0, '', ''),
+            (0, '', ''),
+            (0, '', ''),
+        ]
+
+        mock_tmpfile_copy.side_effect = [
+            '/tmp/mocked_kubeconfig',
+        ]
+
+        mock_oc_binary.side_effect = [
+            'oc',
+        ]
+
+        results = Router.run_ansible(params, False)
+
+        self.assertTrue(results['changed'])
+        for result in results['results']['results']:
+            self.assertEqual(result['returncode'], 0)
+
+        # Need any_order=True (second parameter to assert_has_calls)
+        # because the order of the oc create/replace commands is
+        # non-deterministic.
+        mock_cmd.assert_has_calls([
+            mock.call(['oc', 'get', 'dc', 'router', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'get', 'svc', 'router', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'get', 'sa', 'router', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'get', 'secret', 'router-certs', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'get', 'clusterrolebinding', 'router-router-role', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'adm', 'router', 'router', '--external-host-insecure=False',
+                       "--labels=another-label=val,router=router",
+                       '--ports=80:80,443:443', '--replicas=2', '--selector=type=infra', '--service-account=router',
+                       '--stats-port=1936', '--dry-run=True', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'replace', '-f', mock.ANY, '-n', 'default'], None),
+            mock.call(['oc', 'create', '-f', mock.ANY, '-n', 'default'], None),
+            mock.call(['oc', 'create', '-f', mock.ANY, '-n', 'default'], None),
+            mock.call(['oc', 'create', '-f', mock.ANY, '-n', 'default'], None)], True)
+
+    old_service = '''{
+    "kind": "Service",
+    "apiVersion": "v1",
+    "metadata": {
+        "name": "router",
+        "labels": {
+            "router": "router"
+        }
+    },
+    "spec": {
+        "ports": [
+            {
+                "name": "80-tcp",
+                "protocol": "TCP",
+                "port": 80,
+                "targetPort": 80
+            },
+            {
+                "name": "443-tcp",
+                "protocol": "TCP",
+                "port": 443,
+                "targetPort": 443
+            },
+            {
+                "name": "1936-tcp",
+                "protocol": "TCP",
+                "port": 1936,
+                "targetPort": 1936
+            }
+        ],
+        "selector": {
+            "router": "router"
+        }
+    }
+}'''
+
+    @mock.patch('oc_adm_router.locate_oc_binary')
+    @mock.patch('oc_adm_router.Utils._write')
+    @mock.patch('oc_adm_router.Utils.create_tmpfile_copy')
+    @mock.patch('oc_adm_router.Router._run')
+    def test_create_with_old_service(self, mock_cmd, mock_tmpfile_copy, mock_write, mock_oc_binary):
+        ''' Testing create with old service lacking annotation '''
+        params = {'state': 'present',
+                  'debug': False,
+                  'namespace': 'default',
+                  'name': 'router',
+                  'default_cert': None,
+                  'cert_file': None,
+                  'key_file': None,
+                  'cacert_file': None,
+                  'labels': {"router": "router", "another-label": "val"},
+                  'ports': ['80:80', '443:443'],
+                  'images': None,
+                  'latest_images': None,
+                  'clusterip': None,
+                  'portalip': None,
+                  'session_affinity': None,
+                  'service_type': None,
+                  'kubeconfig': '/etc/origin/master/admin.kubeconfig',
+                  'replicas': 2,
+                  'selector': 'type=infra',
+                  'service_account': 'router',
+                  'router_type': None,
+                  'host_network': None,
+                  'extended_validation': True,
+                  'external_host': None,
+                  'external_host_vserver': None,
+                  'external_host_insecure': False,
+                  'external_host_partition_path': None,
+                  'external_host_username': None,
+                  'external_host_password': None,
+                  'external_host_private_key': None,
+                  'stats_user': None,
+                  'stats_password': None,
+                  'stats_port': 1936,
+                  'edits': []}
+
+        mock_cmd.side_effect = [
+            (1, '', 'Error from server (NotFound): deploymentconfigs "router" not found'),
+            (0, RouterTest.old_service, ''),
+            (1, '', 'Error from server (NotFound): serviceaccount "router" not found'),
+            (1, '', 'Error from server (NotFound): secret "router-certs" not found'),
+            (1, '', 'Error from server (NotFound): clusterrolebinding "router-router-role" not found'),
+            (0, RouterTest.dry_run, ''),
+            (0, '', ''),
+            (0, '', ''),
+            (0, '', ''),
+            (0, '', ''),
+            (0, '', ''),
+        ]
+
+        mock_tmpfile_copy.side_effect = [
+            '/tmp/mocked_kubeconfig',
+        ]
+
+        mock_oc_binary.side_effect = [
+            'oc',
+        ]
+
+        results = Router.run_ansible(params, False)
+
+        self.assertTrue(results['changed'])
+        for result in results['results']['results']:
+            self.assertEqual(result['returncode'], 0)
+
+        # Need any_order=True (second parameter to assert_has_calls)
+        # because the order of the oc create/replace commands is
+        # non-deterministic.
+        mock_cmd.assert_has_calls([
+            mock.call(['oc', 'get', 'dc', 'router', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'get', 'svc', 'router', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'get', 'sa', 'router', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'get', 'secret', 'router-certs', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'get', 'clusterrolebinding', 'router-router-role', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'adm', 'router', 'router', '--external-host-insecure=False',
+                       "--labels=another-label=val,router=router",
+                       '--ports=80:80,443:443', '--replicas=2', '--selector=type=infra', '--service-account=router',
+                       '--stats-port=1936', '--dry-run=True', '-o', 'json', '-n', 'default'], None),
+            mock.call(['oc', 'create', '-f', mock.ANY, '-n', 'default'], None),
+            mock.call(['oc', 'replace', '-f', mock.ANY, '-n', 'default'], None),
+            mock.call(['oc', 'create', '-f', mock.ANY, '-n', 'default'], None),
+            mock.call(['oc', 'create', '-f', mock.ANY, '-n', 'default'], None)], True)
+
     @unittest.skipIf(six.PY3, 'py2 test only')
     @mock.patch('os.path.exists')
     @mock.patch('os.environ.get')
