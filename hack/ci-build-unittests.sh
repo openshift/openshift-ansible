2264b6dacc9a83f5ef718f04b07fe2051ce1b6c3
Patrick Dillon <padillon@redhat.com>
2019-08-27 10:23:39 -0400

    Use updated pip version in test container.
    According to https://github.com/chainer/chainer/issues/7937 the pip in CentOS is out of date, which causes an error we are seeing in unit tests. We can get the new version by skipping the package manager and downloading directly.

--- hack/ci-build-unittests.sh
+++ hack/ci-build-unittests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # This script installs tox dependencies in the test container
-yum install -y gcc libffi-devel python-devel openssl-devel python-pip
+yum install -y gcc libffi-devel python-devel openssl-devel
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
 pip install tox
 chmod uga+w /etc/passwd
