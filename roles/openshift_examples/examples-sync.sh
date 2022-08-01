e9a6a344ff60aa2c75746f35ada39d4c09163364
spolti <fspolti@redhat.com>
2022-08-01 18:41:05 -0300

    [KIECLOUD-586] - Update RHPAM and RHDM templates and imagestreams on OpenShift Service Catalog for 7.13.0 release
    Signed-off-by: spolti <fspolti@redhat.com>

--- roles/openshift_examples/examples-sync.sh
+++ roles/openshift_examples/examples-sync.sh
@@ -8,8 +8,7 @@
 XPAAS_VERSION=ose-v1.4.18
 RHDS_6_TAG=6.4.12.GA
 RHIPS_6_TAG=6.4.12.GA
-RHDM_7_TAG=7.12.1.GA
-RHPAM_7_TAG=7.12.1.GA
+RHPAM_7_TAG=7.13.0.GA
 DG_VERSION=7.3-v1.0
 ORIGIN_VERSION=${1:-v3.11}
 ORIGIN_BRANCH=${2:-release-3.11}
@@ -25,7 +24,6 @@ fi
 wget https://github.com/openshift/origin/archive/${ORIGIN_BRANCH}.zip -O origin.zip
 wget https://github.com/jboss-fuse/application-templates/archive/GA.zip -O fis-GA.zip
 wget https://github.com/jboss-openshift/application-templates/archive/${XPAAS_VERSION}.zip -O application-templates-master.zip
-wget https://github.com/jboss-container-images/rhdm-7-openshift-image/archive/${RHDM_7_TAG}.zip -O rhdm-templates.zip
 wget https://github.com/jboss-container-images/rhpam-7-openshift-image/archive/${RHPAM_7_TAG}.zip -O rhpam-templates.zip
 wget https://github.com/jboss-container-images/jboss-decisionserver-6-openshift-image/archive/${RHDS_6_TAG}.zip -O rhds-templates.zip
 wget https://github.com/jboss-container-images/jboss-processserver-6-openshift-image/archive/${RHIPS_6_TAG}.zip -O rhips-templates.zip
@@ -33,7 +31,6 @@ wget https://github.com/3scale/rhamp-openshift-templates/archive/${RHAMP_TAG}.zi
 wget https://github.com/jboss-container-images/jboss-datagrid-7-openshift-image/archive/${DG_VERSION}.zip -O dg-application-templates.zip
 unzip origin.zip
 unzip application-templates-master.zip
-unzip rhdm-templates.zip
 unzip rhpam-templates.zip
 unzip rhds-templates.zip
 unzip rhips-templates.zip
@@ -45,8 +42,7 @@ mv origin-${ORIGIN_BRANCH}/examples/quickstarts/*.{yaml,json} ${EXAMPLES_BASE}/q
 mv origin-${ORIGIN_BRANCH}/examples/jenkins/jenkins-*template.json ${EXAMPLES_BASE}/quickstart-templates/
 mv origin-${ORIGIN_BRANCH}/examples/image-streams/*.{yaml,json} ${EXAMPLES_BASE}/image-streams/
 mv application-templates-${XPAAS_VERSION}/jboss-image-streams.json ${EXAMPLES_BASE}/xpaas-streams/
-mv rhdm-7-openshift-image-${RHDM_7_TAG}/rhdm712-image-streams.yaml ${EXAMPLES_BASE}/xpaas-streams/
-mv rhpam-7-openshift-image-${RHPAM_7_TAG}/rhpam712-image-streams.yaml ${EXAMPLES_BASE}/xpaas-streams/
+mv rhpam-7-openshift-image-${RHPAM_7_TAG}/rhpam713-image-streams.yaml ${EXAMPLES_BASE}/xpaas-streams/
 mv jboss-decisionserver-6-openshift-image-${RHDS_6_TAG}/templates/decisionserver64-image-stream.json ${EXAMPLES_BASE}/xpaas-streams/
 mv jboss-processserver-6-openshift-image-${RHIPS_6_TAG}/templates/processserver64-image-stream.json ${EXAMPLES_BASE}/xpaas-streams/
 mv jboss-datagrid-7-openshift-image-${DG_VERSION}/templates/datagrid73-image-stream.json ${EXAMPLES_BASE}/xpaas-streams/
@@ -58,8 +54,8 @@ mv application-templates-GA/fis-console-namespace-template.json application-temp
 mv application-templates-GA/fuse-apicurito.yml ${EXAMPLES_BASE}/xpaas-templates/
 find application-templates-${XPAAS_VERSION}/ -name '*.json' ! -wholename '*secret*' ! -wholename '*demo*' ! -wholename '*image-stream.json' ! -name '*processserver6*' ! -name '*decisionserver6*' -exec mv {} ${EXAMPLES_BASE}/xpaas-templates/ \;
 find application-templates-${XPAAS_VERSION}/ -name '*image-stream.json' ! -name '*processserver6*-image-stream*' ! -name '*decisionserver6*-image-stream*' -exec mv {} ${EXAMPLES_BASE}/xpaas-streams/ \;
-find rhdm-7-openshift-image-${RHDM_7_TAG}/templates -name '*.yaml' -exec mv {} ${EXAMPLES_BASE}/xpaas-templates/ \;
-find rhpam-7-openshift-image-${RHPAM_7_TAG}/templates -name '*.yaml' -exec mv {} ${EXAMPLES_BASE}/xpaas-templates/ \;
+find rhpam-7-openshift-image-${RHPAM_7_TAG}/templates/process -name '*.yaml' -exec mv {} ${EXAMPLES_BASE}/xpaas-templates/ \;
+find rhpam-7-openshift-image-${RHPAM_7_TAG}/templates/decision -name '*.yaml' -exec mv {} ${EXAMPLES_BASE}/xpaas-templates/ \;
 find jboss-decisionserver-6-openshift-image-${RHDS_6_TAG}/templates/ -name '*.json' ! -name '*image-stream*'  -exec mv -v {} ${EXAMPLES_BASE}/xpaas-templates/ \;
 find jboss-processserver-6-openshift-image-${RHIPS_6_TAG}/templates/ -name '*.json' ! -name '*image-stream*'  -exec mv -v {} ${EXAMPLES_BASE}/xpaas-templates/ \;
 find 3scale-amp-openshift-templates-${RHAMP_TAG}/ -name '*.yml' -exec mv {} ${EXAMPLES_BASE}/quickstart-templates/ \;
